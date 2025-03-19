15a430f1cde685c7537b32974e0fd8f65b58fbc5
Matheus Arakaki <arakakimath@gmail.com>
2025-03-18 21:28:49 -0400

    feat: avatar component

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,10 +1,11 @@
 import { ThumbsUp, Trash } from 'phosphor-react'
 import styles from './Comment.module.css'
+import { Avatar } from './Avatar'
 
 export function Comment() {
   return (
     <div className={styles.comment}>
-      <img src="https://github.com/arakakimath.png" alt="" className={styles.avatar} />
+      <Avatar hasBorder={false} src='https://github.com/arakakimath.png' />
 
       <div className={styles.commentBox}>
         <div className={styles.commentContent}>
